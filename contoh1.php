da7c1985bd129adee5f320a9f4a4ecea065747d7
mndadela <ad1422908@gmail.com>
2023-10-05 14:24:10 +0700

    tugas pertemuan ke-2

---
+++ contoh1.php
@@ -0,0 +1,11 @@
+<?php
+class contoh1 extends CI_Controller
+{
+    public function index()
+    {
+        echo "<h1 align = center><b>PERKENALKAN</b></h1>";
+        echo "<h2 align = center>Nama Saya Achmad Farhan Surachman
+              Saya tinggal di jakarta Pusat
+              olahraga yg disukai voli</h2>";
+    }
+}
